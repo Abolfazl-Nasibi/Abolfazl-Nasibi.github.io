83b554c762ea662d493fb92d1ebe5e4be381dd10
Abolfazl-Nasibi <abolfazlnasibi2024@gmail.com>
2024-09-10 01:07:37 +0330

    Add files via upload

--- mobileScripts.js
+++ mobileScripts.js
@@ -1,11 +1,29 @@
 
 if (window.innerWidth < 750){
     const navbarElem = document.querySelector('.top-nav')
+    const hamburgerElem = document.querySelector('.hambElem')
+    const navbarLinksElem = document.querySelector('.top-nav-links-ul')
+
+    let hambFlag = false;
+
     window.addEventListener('scroll' , ()=>{
         if(window.scrollY > 250){
             navbarElem.classList.add('scrolledMobile')
         }else{
             navbarElem.classList.remove('scrolledMobile')
+        }
+    })
+
+    hamburgerElem.addEventListener('click' , ()=>{
+        navbarLinksElem.classList.add('active')
+        if(!hambFlag){
+            navbarLinksElem.style.display = 'flex'
+            navbarElem.classList.add('opened')
+            hambFlag = true;
+        }else{
+            navbarLinksElem.style.display = 'none'
+            navbarElem.classList.remove('opened')
+            hambFlag = false;
         }
     })
 }
